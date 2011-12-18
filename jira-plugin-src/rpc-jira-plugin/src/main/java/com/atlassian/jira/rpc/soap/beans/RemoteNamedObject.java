810c8545303610b22a4d3151f46e4a50689e755e
fa_loo@yahoo.com <fa_loo@yahoo.com@9ed66561-ee3c-4afc-029c-e6554c93f712>
2011-12-18 08:02:55 +0000

    changes for v1.0

--- jira-plugin-src/rpc-jira-plugin/src/main/java/com/atlassian/jira/rpc/soap/beans/RemoteNamedObject.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.jira.rpc.soap.beans;
-
-public class RemoteNamedObject extends AbstractNamedRemoteEntity
-{
-    public RemoteNamedObject(String id, String name)
-    {
-        super(id, name);
-    }
-}
